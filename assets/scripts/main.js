b45d04315390d0c5622f9fd3c5204fd0ccb1a83a
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2025-03-02 21:20:59 +0500

    contac the end

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -29,6 +29,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // select
 
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tab");
+  const forms = document.querySelectorAll(".form");
+
+  tabs.forEach(tab => {
+      tab.addEventListener("click", () => {
+          tabs.forEach(t => t.classList.remove("active"));
+          forms.forEach(f => f.classList.remove("active"));
+
+          tab.classList.add("active");
+          document.getElementById(tab.dataset.tab).classList.add("active");
+      });
+  });
+});
+
+
+    // Input validation
+    textInputs.forEach(input => {
+      input.addEventListener("input", function () {
+          if (/\d/.test(this.value)) {  // Agar raqam bo‘lsa
+              this.style.border = "2px solid red";
+              this.nextElementSibling.textContent = "Faqat harflar kiritish mumkin!";
+          } else {
+              this.style.border = "2px solid #ccc"; // Normal holatga qaytarish
+              this.nextElementSibling.textContent = "";
+          }
+      });
+  });
+
 let swiper = new Swiper(".clientSwiper", {
   slidesPerView: 1,
   spaceBetween: 20,
